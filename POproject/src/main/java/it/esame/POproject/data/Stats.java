d2d58ea8f4a1b7dade935203e1adf822afb48e3d
ValerioDAngelo <64924364+ValerioDAngelo@users.noreply.github.com>
2020-07-16 19:24:21 +0200

    Add files via upload
    Progetto completato

---
+++ POproject/src/main/java/it/esame/POproject/data/Stats.java
@@ -0,0 +1,37 @@
+package it.esame.POproject.data;
+
+import it.esame.POproject.exception.DivisionByZeroException;
+
+/** Interfaccia che descrive statistiche generiche
+ *  da applicare ai dati
+ *
+ */
+
+public interface Stats {
+
+/** Il metodo CalcStats() è stato pensato per richiamare tutti gli altri, 
+ *  ovvero i metodi che calcolano le statistiche (min, max)
+ * @throws DivisionByZeroException 
+ * 
+ */
+		
+void CalcStats() throws DivisionByZeroException;
+
+
+/** Il metodo CalcMin() calcola il minimo del dato selezionato.
+ *  
+ */
+
+
+String CalcMin();
+
+
+/** Il metodo CalcMax() calcola il massimo del dato selezionato.
+ * 
+ * 
+ */
+
+
+String CalcMax();
+
+}
